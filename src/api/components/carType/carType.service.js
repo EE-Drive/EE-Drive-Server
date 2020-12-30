76a1984d207df3f89f916ddbbea22a17724f4c64
Amnon Ashkenazy <amnon3234@gmail.com>
2020-12-30 02:11:12 +0200

    adding services

---
+++ src/api/components/carType/carType.service.js
@@ -0,0 +1,61 @@
+
+const CarTypeModel = require('./carType.model');
+
+/**
+ * Used to fetch all carTypes data from the DB
+ * 
+ * @resolve carTypes data
+ */
+module.exports.getCarTypes = () => {
+    return new Promise((resolve, reject) => {
+        CarTypeModel
+            .find()
+            .then(resolve)
+            .catch(reject)
+    });
+};
+
+/**
+ * Used to insert a cartType to the DB
+ * 
+ * @param {object} newCarType object contains the carType companyName, brandName and year
+ * @resolve true if successful
+ */
+module.exports.addCarType = ({companyName, brandName, year}) => {
+    return new Promise((resolve, reject) => {
+        new CarTypeModel({companyName, brandName, year})
+            .save()
+            .then(() => resolve(true))
+            .catch(reject)
+    });
+};
+
+/**
+ * Used to fetch a specific carType from the DB.
+ * 
+ * @param {String} carTypeId 
+ * @resolve requested carType data
+ */
+module.exports.getSpecificCarType = carTypeId => {
+    return new Promise((resolve, reject) => {
+        CarTypeModel
+            .findById(carTypeId)
+            .then(resolve)
+            .catch(reject)
+    });
+};
+
+/**
+ * Used to delete a specific car type from the DB.
+ * 
+ * @param {*} carTypeId 
+ * @resolve true if successful
+ */
+module.exports.deleteSpecificCarType = carTypeId => {
+    return new Promise((resolve, reject) => {
+        CarTypeModel
+            .findByIdAndDelete(carTypeId)
+            .then(() => resolve(true))
+            .catch(reject);
+    });
+};
