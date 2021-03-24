889d8a903cf8b3ddef2b284de0465ff77530a6bb
Amnon Ashkenazy <amnon3234@gmail.com>
2021-03-24 17:28:52 +0200

    adding engine displacement property to car type

--- src/api/components/carType/carType.controller.js
+++ src/api/components/carType/carType.controller.js
@@ -1,5 +1,6 @@
 
 const GenericModelController = require('../../../services/genericModelController.util');
+const {validateKeysInObject} = require('../../..//services/validations.util');
 const carTypeService = require('./carType.service');
 const {SUCCESS_MESSAGES, ERROR_MESSAGES} = require('../../../services/messages.util');
 const Logger = require('../../../config/logger.util');
@@ -8,6 +9,28 @@ const MODEL_NAME = 'Car Type';
 const mustProperties = ['companyName', 'brandName', 'year'];
 const allowedPropertiesToUpdate = ['companyName', 'brandName', 'year', 'drivesID', 'modelsID'];
 const carTypeController = GenericModelController(MODEL_NAME, carTypeService, mustProperties, allowedPropertiesToUpdate); 
+
+
+/**
+ * Used to add a new Car Type to the DB.
+ * 
+ * @respond added item id 
+ */
+carTypeController.addItem = async (req, res) => {
+    try{
+        Logger.databaseQuery(`adding an item to ${MODEL_NAME} -> ${JSON.stringify(req.body)}`);
+        validateKeysInObject(mustProperties, req.body);
+        const newItem = mustProperties.reduce((prev, current) => ({...prev, [current]:req.body[current]}), {}); 
+        if(req.body.engineDisplacement) newItem.engineDisplacement = req.body.engineDisplacement;
+        const savedItem = await carTypeService.addItem(newItem);
+        Logger.databaseResult(`added to ${MODEL_NAME} -> ${JSON.stringify(savedItem)}`);
+        res.status(200).json({createdItemId:savedItem._id, message:SUCCESS_MESSAGES.POST(MODEL_NAME)});
+
+    } catch (err){
+        Logger.databaseError(`Faild to add to ${MODEL_NAME} -> ${err?.message}`);
+        res.status(400).json({ message: ERROR_MESSAGES.POST(MODEL_NAME)});
+    }
+}
 
 /**
  * Used to add a drive and its rawData to a specific carType from the DB.
