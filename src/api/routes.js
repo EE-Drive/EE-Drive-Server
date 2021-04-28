8a11c43c245bb066bb18f9082efe88cb2a3e77c6
Amnon Ashkenazy <amnon3234@gmail.com>
2021-04-28 20:30:17 +0300

    leave me alone

--- src/api/routes.js
+++ src/api/routes.js
@@ -5,11 +5,14 @@ const driveRoute = require("./components/drive/drive.route");
 const modelRouteRoute = require("./components/modelRoute/modelRoute.route");
 const optimalModelRoute = require("./components/optimalModel/optimalModel.route");
 const { ROUTES } = require("../config/global.constants");
+const rawDataModel = require("./components/drive/rawData.model");
 
 router.use(ROUTES.CAR_TYPE_ROUTE, carTypeRoute);
 router.use(ROUTES.DRIVE_ROUTE, driveRoute);
 router.use(ROUTES.MODEL_ROUTE_ROUTE, modelRouteRoute);
 router.use(ROUTES.OPTIMAL_MODEL_ROUTE, optimalModelRoute);
+
+router.get('/raw-data', (req, res) => rawDataModel.find().then(res.json));
 
 module.exports = router;
 
