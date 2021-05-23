950677583095f7f06b16c2e31d0d4b884f7befcf
Amnon Ashkenazy <amnon3234@gmail.com>
2021-05-23 18:23:55 +0300

    quick fix

--- src/api/components/drive/drive.service.js
+++ src/api/components/drive/drive.service.js
@@ -61,6 +61,6 @@ driveService.getDrivesDataForSpecificRoute = (routeID, carTypeId) =>
     .then(drives => drives.filter(drive => drive.carTypeId === carTypeId))
     .then(drives => extractData(drives, routeID));
 
-driveService.getDrivesFromCatType = (carTypeId) => driveModel.find({carTypeId}).then(({ _id, createdAt, driverAssist }) => ({_id, createdAt, driverAssist}));
+driveService.getDrivesFromCatType = (carTypeId) => driveModel.find({carTypeId}).then((res) => res.reduce((prev, {_id, createdAt, driverAssist}) => [...prev, {_id, createdAt, driverAssist}], []));
 
 module.exports = driveService;
